3a36d3592064cddfb3fc35faf3a37ce7329a766c
Gabriel Miranda Carvalho <gabrielm.car@gmail.com>
2024-09-23 01:33:19 -0300

    feat: creating lambda create user

--- internal/domain/usecase.go
+++ internal/domain/usecase.go
@@ -7,17 +7,27 @@ type CustomerOutput struct {
 	Cpf   string `json:"cpf,omitempty"`
 }
 
-type GetCustomerIdUC struct {
+type CreateCustomerInput struct {
+	Name  string `json:"name,omitempty"`
+	Email string `json:"email,omitempty"`
+	Cpf   string `json:"cpf,omitempty"`
+}
+
+type GetCustomerUC struct {
 	customerService CustomerService
 }
 
-func NewGetCustomerIdUC(customerService CustomerService) *GetCustomerIdUC {
-	return &GetCustomerIdUC{
+type CreateCustomerUC struct {
+	customerService CustomerService
+}
+
+func NewGetCustomerUC(customerService CustomerService) *GetCustomerUC {
+	return &GetCustomerUC{
 		customerService: customerService,
 	}
 }
 
-func (uc *GetCustomerIdUC) Execute(cpf string) (*CustomerOutput, error) {
+func (uc *GetCustomerUC) Execute(cpf string) (*CustomerOutput, error) {
 	validCpf, err := NewCPF(cpf)
 	if err != nil {
 		return nil, err
@@ -31,5 +41,28 @@ func (uc *GetCustomerIdUC) Execute(cpf string) (*CustomerOutput, error) {
 		Name: customer.GetName(),
 		Email: customer.GetEmail().Value(),
 		Cpf: customer.GetCPF().Value(),
+	}, nil
+}
+
+func NewCreateCustomerUC(customerService CustomerService) *CreateCustomerUC {
+	return &CreateCustomerUC{
+		customerService: customerService,
+	}
+}
+
+func (uc *CreateCustomerUC) Execute(input *CreateCustomerInput) (*CustomerOutput, error) {
+	customer, err := CreateCustomer(input.Name, input.Email, input.Cpf)
+	if err != nil {
+		return nil, err
+	}
+	customerCreated, err := uc.customerService.CreateCustomer(customer)
+	if err != nil {
+		return nil, err
+	}
+	return &CustomerOutput{
+		Id: customerCreated.GetId(),
+		Name: customerCreated.GetName(),
+		Email: customerCreated.GetEmail().Value(),
+		Cpf: customerCreated.GetCPF().Value(),
 	}, nil
 }
